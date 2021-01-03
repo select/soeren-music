4169a61b7e0af91eeff83b6cef1719e85ffb3b3d
Fa!ko <falko@webpgr.com>
2021-01-03 19:03:51 +0100

    feat: 🎸 load about text from content

--- pages/about.vue
+++ pages/about.vue
@@ -3,25 +3,19 @@
 		<img src="img/Wiese.jpg" alt="" />
 		<section id="main-1" class="about-text padding-header mb-6">
 			<h1 class="title mb-4">About</h1>
-			<p class="mb-4">
-				Adipisicing duis cillum eiusmod non laboris dolore proident labore dolor
-				fugiat aliquip ad deserunt magna est reprehenderit elit aliquip sint
-				dolore commodo dolore dolore ad magna culpa in ut tempor ut tempor
-				tempor aliquip proident sed in officia eu irure qui fugiat qui
-				exercitation veniam elit reprehenderit mollit commodo pariatur do id
-				eiusmod in consequat laboris in incididunt sint excepteur nulla sit ut
-				deserunt incididunt do officia velit anim est fugiat duis ea mollit id
-				in in consequat est sit commodo eiusmod. <br />
-				<br />
-				Lorem ipsum sed laborum incididunt reprehenderit tempor exercitation
-				irure minim pariatur laborum dolore exercitation ut magna adipisicing
-				nisi non dolore in non in mollit eu ut sint in commodo labore occaecat
-				nostrud.
-			</p>
+			<nuxt-content :document="content" />
 			<b-button type="is-primary">Mehr lesen</b-button>
 		</section>
 	</div>
 </template>
+<script>
+export default {
+	async asyncData({ $content, params }) {
+		const content = await $content('about').fetch()
+		return { content }
+	},
+}
+</script>
 <style lang="sass" scoped>
 .about
 	background: rgb(60,66,45)
